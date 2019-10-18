b57eb822bc5d68a1a8e77765b8b11568b1a3f0d7
Phil <phil@Phils-MacBook-Pro.local>
2019-10-18 09:56:19 -0400

    working selection sort

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getMergeSortAnimations } from '../mergeSort';
+import { selectionSort } from '../selectionSort';
 
 const SECONDARY_COLOR = 'red';
 
@@ -8,21 +9,27 @@ const ANIMATION_SPEED_MS = 7;
 const Graph = () => {
     const [array, setArray] = useState([]);
     const [fillColor, setFillColor] = useState('#8884d8');
+    // const [ isFirst, setIsFirst ] = useState(true);
 
     useEffect(() => {
         generateArray();
     }, []);
 
+    // useEffect(() => {
+    //     setArray(array);
+    // }, [array]);
+
     const generateArray = () => {
         let newArray = [];
-        for (let i = 0; i <= 100; i++) {
-            newArray.push(Math.floor(Math.random() * (800 - 5) + 5));
+        for (let i = 0; i <= 50; i++) {
+            newArray.push(Math.floor(Math.random() * (500 - 5) + 5));
         }
         setArray(newArray);
     };
 
     const mergeSort = () => {
         const animations = getMergeSortAnimations(array);
+        console.log('animations', animations);
         for (let i = 0; i < animations.length; i++) {
             const arrayBars = document.getElementsByClassName('array-bar');
             const isColorChange = i % 3 !== 2;
@@ -42,6 +49,33 @@ const Graph = () => {
                     barOneStyle.height = `${newHeight}px`;
                 }, i * ANIMATION_SPEED_MS);
             }
+        }
+    };
+
+    const selectionSort = arr => {
+        // [2,5,4, 3, 1]
+        const arrayBars = document.getElementsByClassName('array-bar');
+        var minIdx,
+            temp,
+            firstColor,
+            secondColor,
+            len = arr.length;
+        for (var i = 0; i < len; i++) {
+            minIdx = i;
+
+            for (var j = i + 1; j < len; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            const firstBar = arrayBars[i].style;
+            const sencondBar = arrayBars[minIdx].style;
+            temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+
+            firstBar.height = `${arr[i]}px`;
+            sencondBar.height = `${arr[minIdx]}px`;
         }
     };
 
@@ -75,6 +109,9 @@ const Graph = () => {
             </button>
             <button style={{ marginTop: '20px', marginBottom: '20px' }} onClick={mergeSort}>
                 Merge Sort
+            </button>
+            <button style={{ marginTop: '20px', marginBottom: '20px' }} onClick={() => selectionSort(array)}>
+                Selection Sort
             </button>
         </React.Fragment>
     );
